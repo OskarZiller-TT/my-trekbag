70f74e50d5709be9fabb0eb55e8e4a92b83889b5
OskarZiller-TT <262096419+OskarZiller-TT@users.noreply.github.com>
2026-03-02 10:43:45 +0000

    Initial commit

---
+++ src/lib/constants.js
@@ -0,0 +1,17 @@
+export const initialItems = [
+  {
+    id: 1,
+    name: "test item 1",
+    packed: true,
+  },
+  {
+    id: 2,
+    name: "test item 2",
+    packed: false,
+  },
+  {
+    id: 3,
+    name: "test item 3",
+    packed: false,
+  },
+];
