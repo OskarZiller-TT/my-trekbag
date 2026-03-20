2617dbde3682b13dd05086bd7fd3a1b191e413e3
OskarZiller-TT <262096419+OskarZiller-TT@users.noreply.github.com>
2026-03-20 10:14:37 +0000

    fix: use children pattern in ButtonGroup to fix labels

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 export default function ButtonGroup() {
   const markAllAsComplete = useItemsStore((state) => state.markAllAsComplete);
   const markAllAsIncomplete = useItemsStore(
-    (state) => state.markAllAsIncomplete
+    (state) => state.markAllAsIncomplete,
   );
   const resetToInitial = useItemsStore((state) => state.resetToInitial);
   const removeAllItems = useItemsStore((state) => state.removeAllItems);
@@ -32,11 +32,12 @@ export default function ButtonGroup() {
     <section className="button-group">
       {secondaryButtons.map((button) => (
         <Button
-          key={button.text + button.onClick.toString()}
-          text={button.text}
+          key={button.text}
           onClick={button.onClick}
           buttonType="secondary"
-        />
+        >
+          {button.text}
+        </Button>
       ))}
     </section>
   );
